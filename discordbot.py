69c7b94108cad6e33e3a0e8d9968cb4e879ce39c
Brandon <98434819+Brandon2023@users.noreply.github.com>
2022-02-23 17:32:56 -0800

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -5,7 +5,7 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    print("Bot has connected to discord!")
+    print("Bot has connected to discord")
 
 @client.event
 async def on_message(message):

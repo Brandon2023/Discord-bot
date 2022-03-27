54c127970b37e6d6c25b3d79470e61a503ac3b98
Brandon <98434819+Brandon2023@users.noreply.github.com>
2022-03-27 16:25:08 -0700

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,7 +1,8 @@
 import discord 
+import time
 from discord.ext import commands
 
-token = ''
+token = 'OTQ1ODQwMjM0MDQ0NzM5NTk1.YhWAXA.wDIXnMluro1tY71sNK8vmQHOWzU'
 client = discord.Client()
 
 client = commands.Bot(command_prefix='$')
@@ -11,8 +12,25 @@ async def on_ready():
     print("Bot has connected to discord")
 
 @client.command()
-async def test1(ctx):
-    await ctx.send("test1 is successful")
+async def test(ctx):
+    await ctx.send("its working lol")
+
+@client.command()
+async def help(ctx):
+    await ctx.send("")
+
+@client.command()
+async def thefunny(ctx):
+    while True: 
+        time.sleep(2)
+        await ctx.send("@everyone")   
+
+
+
+
+
+
+
 
 
 client.run(token)
